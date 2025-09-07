bf7bdd07ae656984a08d8dfdf95e131ffaad6fe3
rachit-developer24 <srachit841@gmail.com>
2025-09-07 17:38:50 +0100

    Initial Commit

---
+++ FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+//  Created by Rachit Sharma on 07/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirebaseAppTests/FirebaseAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppTests.swift
+//  FirebaseAppTests
+//
+//  Created by Rachit Sharma on 07/09/2025.
+//
+
+import Testing
+@testable import FirebaseApp
+
+struct FirebaseAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
